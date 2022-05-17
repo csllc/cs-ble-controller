5010b7dbb64edea844850beac2279ee9eb7fecda
Calvin Walden <cwalden@controls.com>
2022-05-17 08:24:42 -0500

    Add Electron app example

---
+++ examples/electron/index.js
@@ -0,0 +1,54 @@
+// Main process
+
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  })
+
+  let webContents = win.webContents;
+
+  webContents.openDevTools();
+
+  webContents.on('did-finish-load', () => {
+    console.log("did-finish-load");
+
+    webContents.send('bleSetup');
+
+  });
+
+  win.loadFile('index.html')
+
+  win.webContents.on('select-bluetooth-device', (event, deviceList, callback) => {
+    event.preventDefault();
+    console.log("deviceList", deviceList);
+    callback(deviceList[0].deviceId);
+
+    // For now, we just grab the first device, but eventually we'll need to implement
+    // a device picker
+  })
+
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+
+  createWindow();
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+
---
+++ examples/electron/renderer.js
@@ -0,0 +1,104 @@
+// Renderer process
+
+const { ipcRenderer } = require('electron');
+const CsMbBle = require('../..');
+
+ipcRenderer.on('bleSetup', () => {
+  console.log("bleSetup()");
+
+  document.getElementById('button-refresh-connections').addEventListener('click', testBluetooth);
+});
+
+// Test bluetooth features
+let testBluetooth = function() {
+
+  console.log("testBluetooth");
+
+  let ble = new CsMbBle({ bluetooth: navigator.bluetooth,
+                          name: 'CS1816' });
+
+
+  ble.on('availabilitychanged', (event) => {
+    console.log("BLE availability changed", event);
+  });
+
+  ble.on('gattserverdisconnected', (event) => {
+    console.log("BLE connection lost", event);
+  });
+
+  ble.on('scanStart', (filter) => {
+    console.log("BLE scanning started, using filter", filter);
+  });
+
+  ble.on('scanStop', () => {
+    console.log("BLE scanning stopped");
+  });
+
+  ble.on('discover', (newDevice) => {
+    console.log("BLE device discovered:", newDevice);
+  });
+
+  ble.on('connecting', () => {
+    console.log("Connecting to BLE device...");
+  });
+
+  ble.on('connected', () => {
+    console.log("Connected to BLE device.");
+  });
+
+  ble.on('disconnecting', () => {
+    console.log("Disconnecting from BLE device...");
+  });
+
+  ble.on('disconnected', () => {
+    console.log("Disconnected from BLE device.");
+  });
+
+  ble.on('inspecting', () => {
+    console.log("Inspecting BLE dongle...");
+  });
+
+  ble.on('inspected', () => {
+    console.log("Inspection complete.");
+
+    ble.getInfo()
+    .then((info) => {
+      console.log("Device information:");
+      console.log("  System ID:             ", info.systemId);
+      console.log("  Manufacturer:          ", info.manufacturerName);
+      console.log("  Model Number:          ", info.modelNumber);
+      console.log("  Serial Number:         ", info.dongleSerialNumber);
+      console.log("  Software Revision:     ", info.softwareRevision);
+      console.log("  Firmware Revision:     ", info.firmwareRevision);
+      console.log("  Hardware Revision:     ", info.hardwareRevision);
+      console.log("  Dongle Modbus ID:      ", info.modbusId);
+      console.log("  Product:               ", info.product);
+      console.log("  Product Serial Number: ", info.serial);
+    });
+  });
+
+  console.log("ble", ble);
+
+  return new Promise((resolve, reject) => {
+    ble.getAvailability()
+    .then(() => {
+      ble.startScanning()
+      .then((device) => {
+        console.log("device", device);
+        resolve({
+          text: `${device.name} (${device.id})`, // TODO: Truncate ID
+          value: device.id,
+          type: "ble",
+          typeText: "Bluetooth Dongle",
+          module: "cs-mb-ble",
+          bus: device,
+          port: `${device.name} (ID ${device.id})`,
+        });
+      });
+    });
+  })
+  .then(() => {
+    ble.open();
+  })
+}
+
